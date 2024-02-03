825e2502ca3b159af3921e819a05009ba1348182
Noorie Esmaili <noorie.esmaili@gmail.com>
2024-02-03 16:18:56 +0100

    update indentation

--- src/main/java/se/lnu/Exercise5/EuclideanMain.java
+++ src/main/java/se/lnu/Exercise5/EuclideanMain.java
@@ -1,30 +1,29 @@
-package ne222hz_assign3.Exercise5;
+package se.lnu.Exercise5;
 
 public class EuclideanMain {
 
-	public static int gcd(int M, int N) {
-		if (M == 0) {
-			return N;
-		}
+    public static int gcd(int M, int N) {
+        if (M == 0) {
+            return N;
+        }
+        return gcd(N % M, M);
+    }
 
-		return gcd(N % M, M);
-	}
+    public static void main(String[] args) {
+        int M = 18;
+        int N = 12, g;
+        g = gcd(M, N);
+        System.out.println("GCD(" + M + "," + N + ") = " + g);
 
-	public static void main(String[] args) {
-		int M = 18;
-		int N = 12, g;
-		g = gcd(M, N);
-		System.out.println("GCD(" + M + "," + N + ") = " + g);
+        M = 42;
+        N = 56;
+        g = gcd(M, N);
+        System.out.println("GCD(" + M + "," + N + ") = " + g);
 
-		M = 42;
-		N = 56;
-		g = gcd(M, N);
-		System.out.println("GCD(" + M + "," + N + ") = " + g);
-
-		M = 9;
-		N = 28;
-		g = gcd(M, N);
-		System.out.println("GCD(" + M + "," + N + ") = " + g);
-	}
+        M = 9;
+        N = 28;
+        g = gcd(M, N);
+        System.out.println("GCD(" + M + "," + N + ") = " + g);
+    }
 
 }
