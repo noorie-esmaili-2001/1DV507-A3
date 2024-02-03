b9246e11962b93b0313050b87736522ec8c9b9a2
Noorie Esmaili <noorie.esmaili@gmail.com>
2024-02-03 12:39:41 +0100

    Update logic of recursive printing

--- src/main/java/se/lnu/Exercise1/PrintRecursive.java
+++ src/main/java/se/lnu/Exercise1/PrintRecursive.java
@@ -1,34 +1,29 @@
-package ne222hz_assign3.Exercise1;
+package se.lnu.Exercise1;
 
 public class PrintRecursive {
-	private static void printReverse(String str, int i) {
-		if ((str==null)||(str.length() <= 1)) 
-	           System.out.println(str); 
-	        else
-	        { 
-	            System.out.print(str.charAt(str.length()-1)); 
-	            printReverse(str.substring(0,str.length()-(i+1)) , i); 
-	        } 
-		
-	}
+    private static void printReverse(String str, int i) {
+        if (str.isEmpty()) {
+            return;
+        }
+        System.out.print(str.charAt(str.length() - 1));
+        printReverse(str.substring(0, str.length() - (i + 1)), i);
 
-	private static void print(String str, int i) {
-		if ((str==null)||(str.length() <= 1)) 
-	           System.out.println(str); 
-	        else
-	        { 
-	            System.out.print(str.charAt(i)); 
-	            print(str.substring(i+1 , str.length()) , i); 
-	        } 
-		
-	}
-	
-	public static void main(String[] args) {
-		   String str = "Hello Everyone!";
-				
-		   print(str, 0);
-		   System.out.println(); // Line break
-		   printReverse(str , 0);
-		}
+    }
+
+    private static void print(String str, int i) {
+        if (str.isEmpty()) {
+            return;
+        }
+        System.out.print(str.charAt(i));
+        print(str.substring(i + 1), i);
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello Everyone!";
+
+        print(str, 0);
+        System.out.println(); // Line break
+        printReverse(str, 0);
+    }
 
 }
